2fb81d184f26568babd08ac1c90170cbee452c80
Will Corcoran <corcoranwill@gmail.com>
2020-03-14 14:31:25 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,14 +1,18 @@
 class MP3Importer
   attr_reader :path
 
+  # accepts a file path to parse mp3 files from
   def initialize(path)
     @path = path
   end
 
+  # loads all mp3 files in the directory
+  # normalizes the filename to just the mp3 filename with no path 
   def files
     @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
   end
 
+  # imports the files into the library by creating songs from a filename 
   def import
     files.each{|f| Song.new_by_filename(f)}
   end
